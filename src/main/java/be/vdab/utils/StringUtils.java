3217d2dd94b9b164f3a2cb0d8db3a40bce6f3902
Aleks <Aleks@LOK111-05>
2017-03-31 08:50:01 +0200

    eerste commit

---
+++ src/main/java/be/vdab/utils/StringUtils.java
@@ -0,0 +1,30 @@
+package be.vdab.utils;
+
+import java.math.BigDecimal;
+
+public class StringUtils {
+    
+    public static boolean isLong(String string) {
+	if (string == null) {
+	    return false;
+	}
+	try {
+	    Long.parseLong(string);
+	    return true;
+	} catch (NumberFormatException ex) {
+	    return false;
+	}
+    }
+
+    public static boolean isBigDecimal(String string) {
+	if (string == null) {
+	    return false;
+	}
+	try {
+	    new BigDecimal(string);
+	    return true;
+	} catch (NumberFormatException ex) {
+	    return false;
+	}
+    }
+}
